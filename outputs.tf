81110e5dd3f3cf0710b059b76c435fe1ea95e6df
rnutzman <45185619+rnutzman@users.noreply.github.com>
2023-08-21 17:33:32 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,12 @@ output "instance_ami" {
 output "instance_arn" {
   value = aws_instance.blog.arn
 }
+
+output "security_group_arn" {
+  value = <terraform security group name>.security_group_arn
+}
+
+output "security_group_name" {
+  value = <terraform security group name>.security_group_name
+}
+
